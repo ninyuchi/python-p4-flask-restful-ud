a02e7eff4418dc6b10c97fab18981298e04cd681
Nasra Kassim <ninyuchinasra@gmail.com>
2023-10-04 18:22:34 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,53 +1,53 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Newsletter
+from models import db, Newsletter  # Import the Newsletter model
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newsletters.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
+class Index(Resource):
 
     def get(self):
-        
+
         response_dict = {
-            "message": "Welcome to the Newsletter RESTful API",
+            "index": "Welcome to the Newsletter RESTful API",
         }
-        
+
         response = make_response(
-            response_dict,
+            jsonify(response_dict),
             200,
         )
 
         return response
 
-api.add_resource(Home, '/')
+api.add_resource(Index, '/')
 
-class Newsletters(Resource):
+class NewsletterResource(Resource):  # Renamed the resource class
 
     def get(self):
-        
+
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
 
         response = make_response(
-            response_dict_list,
+            jsonify(response_dict_list),
             200,
         )
 
         return response
 
     def post(self):
-        
+
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
@@ -59,13 +59,13 @@ class Newsletters(Resource):
         response_dict = new_record.to_dict()
 
         response = make_response(
-            response_dict,
+            jsonify(response_dict),
             201,
         )
 
         return response
 
-api.add_resource(Newsletters, '/newsletters')
+api.add_resource(NewsletterResource, '/newsletters')  # Renamed the resource
 
 class NewsletterByID(Resource):
 
@@ -74,14 +74,46 @@ class NewsletterByID(Resource):
         response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
 
         response = make_response(
-            response_dict,
+            jsonify(response_dict),
             200,
+        )
+
+        return response
+
+    def patch(self, id):
+
+        record = Newsletter.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+
+        record = Newsletter.query.filter_by(id=id).first()
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            jsonify(response_dict),
+            200
         )
 
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
-
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
